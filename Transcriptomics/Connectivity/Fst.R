aef71d7b4665fe7b4548bb0320ae542c955c3103
LisaSkalon <36557899+LisaSkalon@users.noreply.github.com>
2026-07-09 14:12:40 +0300

    refined plot export

--- Transcriptomics/Connectivity/Fst.R
+++ Transcriptomics/Connectivity/Fst.R
@@ -57,7 +57,7 @@ final_df <- final_df[!is.na(final_df$Pop), ]
 genotype.st <- stamppConvert(final_df, "r")
 
 # run Fst
-genotype.fst <- stamppFst(genotype.st, nboots = 1000, percent = 95, nclusters = 4)
+genotype.fst <- stamppFst(genotype.st, nboots = 1000, percent = 95, nclusters = 3)
 
 print(genotype.fst$Fsts)
 print(genotype.fst$Pvalues)
@@ -90,15 +90,23 @@ for(i in 1:n) {
     if(i > j) {
       label_matrix[i, j] <- sprintf("%.3f", fst_mat[i, j]) 
     } else if (i < j) {
-      label_matrix[i, j] <- paste0("p=", sprintf("%.3f", p_mat[i, j]))
+      label_matrix[i, j] <- ifelse(
+        p_mat[i, j] < 0.001,
+        "p<0.001",
+        paste0("p=", sprintf("%.3f", p_mat[i, j]))
+      )
     }
   }
 }
 
 # color 
+breaks <- pretty(range(fst_mat, na.rm = TRUE), n = 5)
+
 col_fun <- colorRamp2(
-  breaks = pretty(range(fst_mat, na.rm = TRUE), n = 5), 
-  colors = colorRampPalette(c("#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#313695"))(length(breaks))
+  breaks = breaks,
+  colors = colorRampPalette(
+    c("#e0f3f8", "#abd9e9", "#74add1", "#4575b4", "#5a5eaa")
+  )(length(breaks))
 )
 
 
@@ -110,8 +118,16 @@ ht <- Heatmap(
   cluster_rows = FALSE,
   cluster_columns = FALSE,
   
+  row_names_gp = gpar(fontsize = 7),
+  column_names_gp = gpar(fontsize = 7),
+  
+  heatmap_legend_param = list(
+    title_gp = gpar(fontsize = 8, fontface = "bold"),
+    labels_gp = gpar(fontsize = 7)
+  ),
+  
   cell_fun = function(j, i, x, y, width, height, fill) {
-
+    
     if(i < j) {
       grid.rect(x, y, width, height, gp = gpar(fill = "white", col = NA))
     }
@@ -119,13 +135,15 @@ ht <- Heatmap(
     label <- label_matrix[i, j]
     if (label != "") {
       text_col <- ifelse(i < j, "grey40", "black")
-      grid.text(label, x, y, gp = gpar(fontsize = 10, col = text_col))
+      grid.text(label, x, y, gp = gpar(fontsize = 6, col = text_col))
     }
-    # diagonal slash
+    
     if (i == j) {
-      grid.lines(x = unit.c(x - 0.5 * width, x + 0.5 * width),
-                 y = unit.c(y + 0.5 * height, y - 0.5 * height),
-                 gp = gpar(col = "grey80"))
+      grid.lines(
+        x = unit.c(x - 0.5 * width, x + 0.5 * width),
+        y = unit.c(y + 0.5 * height, y - 0.5 * height),
+        gp = gpar(col = "grey80")
+      )
     }
   }
 )
@@ -171,48 +189,78 @@ scores <- scores %>%
   left_join(centroids, by = "Pop", suffix = c("", ".centroid"))
 
 # Plot
+
+pt_to_mm <- function(pt) pt / 2.845
+
+condition_colors <- c(
+  "N3"  = "#FF8900",
+  "C3"        = "#FE6100",
+  "T10&T25" = "#AA42B8",
+  "N30" = "#3f51b6"
+)
+
 pca_plot2 <- ggplot(scores, aes(x = Axis1, y = Axis2, color = Pop)) +
-  # segments from sample to centroid
-  geom_segment(aes(xend = Axis1.centroid, yend = Axis2.centroid),
-               alpha = 0.4, linewidth = 0.3) +
-  # centroid labels (drawn first, so underneath points)
-  geom_text(data = centroids, aes(x = Axis1, y = Axis2, label = Pop, color = Pop),
-            , size = 6, show.legend = FALSE, alpha = 0.6) +
-  # points for samples (drawn after, so on top)
-  geom_point(size = 3) +
-  # centroid markers (optional cross shape)
-  #geom_point(data = centroids, aes(x = Axis1, y = Axis2, color = Pop),
-  #           size = 1, shape = 4, stroke = 1.5) +
+  geom_segment(
+    aes(xend = Axis1.centroid, yend = Axis2.centroid),
+    alpha = 0.4,
+    linewidth = 0.3
+  ) +
+  geom_text(
+    data = centroids,
+    aes(x = Axis1, y = Axis2, label = Pop, color = Pop),
+    size = pt_to_mm(8),
+    show.legend = FALSE,
+    alpha = 0.6
+  ) +
+  geom_point(size = 1) +
   labs(
     x = paste0("PC1 (", round(eig[1], 1), "%)"),
     y = paste0("PC2 (", round(eig[2], 1), "%)")
   ) +
   theme_minimal() +
-  theme(legend.position = "none")
+  theme(
+    legend.position = "none",
+    axis.title = element_text(size = 8),
+    axis.text = element_text(size = 7)
+  )+
+  scale_color_manual(values = condition_colors)
 
 pca_plot2
 
 #### save pics ####
-
 ht_grob <- grid.grabExpr(draw(ht))
 
 plotA <- ggdraw(ht_grob)
-
 plotB <- pca_plot2 
 
 final_combined_plot <- plot_grid(
   plotA, plotB, 
   ncol = 2, 
   labels = "AUTO",       
-  label_size = 14,       
+  label_size = 9,       
   rel_widths = c(1, 1) 
 )
 
-# Export the result
-save_plot("Fst_PCA_Combined_Plot.pdf", final_combined_plot, base_width = 10, base_height = 4)
-save_plot("Fst_PCA_Combined_Plot.png", final_combined_plot, base_width = 10, base_height = 4, dpi = 300)
+fig_width_cm <- 14
+fig_height_cm <- 6
+
+save_plot(
+  "Fst_PCA_Combined_Plot.pdf",
+  final_combined_plot,
+  base_width = fig_width_cm / 2.54,
+  base_height = fig_height_cm / 2.54
+)
+
+save_plot(
+  "Fst_PCA_Combined_Plot.png",
+  final_combined_plot,
+  base_width = fig_width_cm / 2.54,
+  base_height = fig_height_cm / 2.54,
+  dpi = 600
+)
 
 plot(final_combined_plot)
 
 
 save.image("050426.Rdata")
+save.image("090726.Rdata")
